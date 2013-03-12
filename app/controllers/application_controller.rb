dbd18ca5d39fc2a226397c98da91e21dbda89726
Celine Ui <celine.ui@gmail.com>
2013-03-12 15:35:35 -0500

    fixed syntax error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def authenticate
     if Rails.env.staging?
       authenticate_or_request_with_http_basic do |username, password|
-        username == ENV["HTTP_USERNAME"] && password == ENV ["HTTP_PASSWORD"]
+        username == ENV["HTTP_USERNAME"] && password == ENV["HTTP_PASSWORD"]
       end
     end
 
